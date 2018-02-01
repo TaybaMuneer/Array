dd535c1be9ee0cc1ef966c1d8b33d7b105768db8
TaybaMuneer <35809328+TaybaMuneer@users.noreply.github.com>
2018-02-01 16:37:02 +0000

    array

---
+++ array-literal.js
@@ -0,0 +1,8 @@
+var colors = new Array("white",
+                       "black",
+					   "red");
+colors.push("purple");
+
+var el = document.getElementById("colors");
+el.textContent = colors[0];
+el.textContent = colors[3];
